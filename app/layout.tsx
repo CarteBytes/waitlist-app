27644fa77ecbc071a1d46da9ef6b7248a25cf084
Allan Serna <emanuallan@gmail.com>
2024-10-16 13:27:38 -0500

    changed jpg to png

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,13 +19,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark" suppressHydrationWarning>
-      <meta property="og:image" content="/social-logo.jpg" />
+      <meta property="og:image" content="/social-logo.png" />
       <meta property="og:image:type" content="image/png" />
       <meta property="og:image:width" content="1280" />
       <meta property="og:image:height" content="832" />
       <meta property="og:site_name" content="CarteBytes | Waitlist" />
       <meta property="og:url" content="https://www.cartebytes.com/" />
-      <meta name="twitter:image" content="/social-logo.jpg" />
+      <meta name="twitter:image" content="/social-logo.png" />
       <meta name="twitter:image:type" content="image/png" />
       <meta name="twitter:image:width" content="1280" />
       <meta name="twitter:image:height" content="832" />
