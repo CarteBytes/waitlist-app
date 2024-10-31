ea98481c3350c5cb106afa8b92b9792d0510557b
Allan Serna <emanuallan@gmail.com>
2024-10-31 18:48:09 -0500

    fixed spacing for edit menu

--- app/menus/playground/components/EditLiberoMenu.tsx
+++ app/menus/playground/components/EditLiberoMenu.tsx
@@ -107,7 +107,7 @@ const ContentPages = ({
       {menu.content?.map((section, i) => {
         return (
           <React.Fragment key={i}>
-            <div className="mt-10 flex h-full min-h-16 w-full items-center justify-center border-y-2 border-black bg-[#F6FE9B] font-bold text-black shadow-xl">
+            <div className="mt-16 flex h-full min-h-16 w-full items-center justify-center border-y-2 border-black bg-[#F6FE9B] font-bold text-black shadow-xl">
               Page {section.page_index + 1}, Section {section.section_index + 1}
             </div>
             <section
@@ -302,7 +302,7 @@ const TitlePage = ({
 
   return (
     <>
-      <div className="mt-10 flex h-full min-h-16 w-full items-center justify-center border-y-2 border-black bg-[#F6FE9B] font-bold text-black shadow-xl">
+      <div className="mt-16 flex h-full min-h-16 w-full items-center justify-center border-y-2 border-black bg-[#F6FE9B] font-bold text-black shadow-xl">
         Title Page
       </div>
       <section
@@ -352,7 +352,7 @@ const TitlePage = ({
 const FooterPage = ({ restaurant }: { restaurant: RestaurantT }) => {
   return (
     <>
-      <div className="mt-10 flex h-full min-h-16 w-full items-center justify-center border-y-2 border-black bg-[#F6FE9B] font-bold text-black shadow-xl">
+      <div className="mt-16 flex h-full min-h-16 w-full items-center justify-center border-y-2 border-black bg-[#F6FE9B] font-bold text-black shadow-xl">
         Footer Page
       </div>
       <section
